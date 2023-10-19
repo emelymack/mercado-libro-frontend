765c4ad66c328227b9ad33c9dd48fd85f2cdf5c3
Elkinssm <elkinsilvamanrique@gmail.com>
2023-10-19 18:20:33 -0500

    Fix main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root')
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
