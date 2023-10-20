6edca099f9d5311de05f02c47d9ded3ddc6ac5eb
Elkinssm <elkinsilvamanrique@gmail.com>
2023-10-19 19:47:35 -0500

    Add dark mode and colors theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import './App.css'
-import AccordionTest from './components/AccordionTest'
+import { Box, Text } from "@chakra-ui/react";
+import "./App.css";
+import ColorModeSwitch from "./components/ColorModeSwitch";
 
 function App() {
-
   return (
     <>
-      <AccordionTest />
+      <ColorModeSwitch />
+      <Box bg="brand.blueLogo" fontFamily="heading">
+        Esto es un texto de prueba de le fuente
+      </Box>
+      <Text color="brand.violetLogo" fontFamily="heading">
+        Texto de Encabezado
+      </Text>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/theme.ts
@@ -0,0 +1,22 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import "@fontsource/gabarito";
+
+const colors = {
+  brand: {
+    blueLogo: "#003844",
+    greenLogo: "#006C67",
+    violetLogo: "#8884FF",
+  },
+};
+
+const fonts = {
+  heading: `'Gabarito', sans-serif`,
+  body: `'Gabarito', sans-serif`,
+};
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
+
+const theme = extendTheme({ config, colors, fonts });
+
+export default theme;
