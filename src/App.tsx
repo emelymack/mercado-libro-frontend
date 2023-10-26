da600a82330c70416b63d17ea78aa63f78e43130
Elkinssm <elkinsilvamanrique@gmail.com>
2023-10-25 21:30:48 -0500

    Add theme and font packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import "./App.css";
 import AccordionTest from "./components/AccordionTest";
 import Layout from "./components/Layout";
+import ColorModeSwitch from "./components/ColorModeSwitch";
 
 function App() {
   return (
-    <Layout>
-      <AccordionTest />
-    </Layout>
+    <ChakraProvider theme={theme}>
+      <>
+        <ColorModeSwitch />
+        <Layout>
+          <AccordionTest />
+        </Layout>
+      </>
+    </ChakraProvider>
   );
 }
 
