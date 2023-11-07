2fb2738f680a8a64c8334e130f9926b853f2d50d
Elkinssm <elkinsilvamanrique@gmail.com>
2023-11-06 23:46:49 -0500

    Add loading state to handleLogout function and UserInfo component

--- src/components/Layout/Header/MyAccount.tsx
+++ src/components/Layout/Header/MyAccount.tsx
@@ -13,39 +13,48 @@ import { useNavigate } from "react-router-dom";
 import { clearLocalStorage } from "../../../utils/setStorage";
 import { useAppDispatch, useAppSelector } from "../../../context/hooks";
 import { logout } from "../../../context/slices/authSlice";
+import { useState } from "react";
+import CustomLoading from "../../CustomLoading/CustomLoading";
 
 const MyAccount = () => {
-  debugger;
   const history = useNavigate();
   const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
   const { name, lastName } = useAppSelector((state) => state.user);
 
   const handleLogout = () => {
-    dispatch(logout());
-    clearLocalStorage();
-    history("/");
+    setIsLoading(true);
+    setTimeout(() => {
+      dispatch(logout());
+      clearLocalStorage();
+      history("/");
+      setIsLoading(false);
+    }, 1000);
   };
   const initials = `${name} ${lastName}`;
 
   return (
-    <Menu>
-      <MenuButton
-        as={Button}
-        bg={"none"}
-        style={{ color: "var(--secondary)" }}
-        ps={3}
-        pe={2}
-        rightIcon={<ChevronDownIcon />}
-      >
-        <Avatar size="sm" name={initials} mr={2} />
-        MI CUENTA
-      </MenuButton>
-      <MenuList>
-        <MenuItem>Mis pedidos</MenuItem>
-        <MenuDivider />
-        <MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
-      </MenuList>
-    </Menu>
+    <>
+      <Menu>
+        <MenuButton
+          as={Button}
+          bg={"none"}
+          style={{ color: "var(--secondary)" }}
+          ps={3}
+          pe={2}
+          rightIcon={<ChevronDownIcon />}
+        >
+          <Avatar size="sm" name={initials} mr={2} />
+          MI CUENTA
+        </MenuButton>
+        <MenuList>
+          <MenuItem>Mis pedidos</MenuItem>
+          <MenuDivider />
+          <MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
+        </MenuList>
+      </Menu>
+      {isLoading ? <CustomLoading /> : null}
+    </>
   );
 };
 
