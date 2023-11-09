04784148b484b49c597fcab61cc1d37e61daf940
Iya Moura <iyamoura@hotmail.com>
2023-11-08 23:25:11 -0300

    categorias menu nav en mobile

--- src/components/Layout/Header/Mobile/NavMenu.tsx
+++ src/components/Layout/Header/Mobile/NavMenu.tsx
@@ -1,8 +1,10 @@
 import { ChevronDownIcon } from '@chakra-ui/icons'
-import { Box, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Stack } from '@chakra-ui/react'
+import { Box, Button, Link, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Stack } from '@chakra-ui/react'
 import NavLink from '../NavLink'
 import Login from '../Login'
 import MyAccount from '../MyAccount'
+import { useEffect, useState } from 'react'
+import { Category, getAllCategory } from '../../../../services/BookService'
 
 interface Props {
   Links: {
@@ -12,6 +14,19 @@ interface Props {
   isLogged: boolean
 }
 const NavMenu = ({Links, isLogged}: Props) => {
+  
+  const [categoria, setCategoria] = useState<Category[]>([]);
+
+  useEffect(() => {
+
+    getAllCategory ()
+    .then((res) => {
+    console.log(res);
+    setCategoria(res)
+    })
+
+  }, []);
+
   return (
     <Box pb={4} display={{ lg: 'none' }} pt={8} >
       <Stack as={'nav'} spacing={4} alignItems={'center'}>
@@ -19,11 +34,17 @@ const NavMenu = ({Links, isLogged}: Props) => {
           <MenuButton as={Button} rightIcon={<ChevronDownIcon />} bg={'none'} color={'var(--secondary)'} fontSize={17}>
             Categorías
           </MenuButton>
+          
           <MenuList>
-            <MenuItem>Link 1</MenuItem>
+          {categoria?.map((item) => (
+                    <MenuItem>
+                      {item?.name}
+                    </MenuItem>
+                    ))}
+           {/*  <MenuItem>Link 1</MenuItem>
             <MenuItem>Link 2</MenuItem>
             <MenuDivider />
-            <MenuItem>Link 3</MenuItem>
+            <MenuItem>Link 3</MenuItem> */}
           </MenuList>
         </Menu>
         {Links.map((link) => (
