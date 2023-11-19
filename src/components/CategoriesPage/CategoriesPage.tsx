5b0e8615b79395075ea8f889b053c398af71f693
Iya Moura <iyamoura@hotmail.com>
2023-11-19 14:26:57 -0300

    detalle string categories

--- src/components/CategoriesPage/CategoriesPage.tsx
+++ src/components/CategoriesPage/CategoriesPage.tsx
@@ -10,119 +10,9 @@ import CustomLoading from "../CustomLoading/CustomLoading";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import BreadcrumbNav from "./BreadcrumbNav";
 
-const Productos = [
-  {
-    id: 1,
-    titulo: "Libro1",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 2,
-    titulo: "Libro2",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 3,
-    titulo: "Libro3",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 4,
-    titulo: "Libro4",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 5,
-    titulo: "Libro5",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 6,
-    titulo: "Libro6",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 7,
-    titulo: "Libro7",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 8,
-    titulo: "Libro8",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 9,
-    titulo: "Libro9",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 10,
-    titulo: "Libro10",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 11,
-    titulo: "Libro11",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-  {
-    id: 12,
-    titulo: "Libro12",
-    imagen:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfhFBFzppgkYJlztgiZ3luEU6q4x3IAyfjPX9cen1HzwseJtfUOiBsM4nXvKfdFkV5e0&usqp=CAU",
-    autor: "Lorem ipsum dolor.",
-    url: "",
-    precio: 10000,
-  },
-];
+
 
 export const Categories = () => {
-  // const params = useParams();
   const {categoryName} = useParams();
   const [ librosCategoria, setLibrosCategoria ] = useState<Book[]>([]);
   const [ isLoading, setIsLoading ] = useState(false)
@@ -150,7 +40,7 @@ export const Categories = () => {
 
   return (
     <Container maxW="container.xl" bg="white.600" mb={20} className={`page ${isScrolling ? 'scroll' : ''}`}>
-      <BreadcrumbNav category={categoryName} />
+      <BreadcrumbNav category={`${categoryName}`} />
       <Heading
         size="3xl"
         fontWeight={900}
