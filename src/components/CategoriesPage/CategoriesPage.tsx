5c1c50f20424154f51cc4608fcbb7d9207b113fd
Elkinssm <elkinsilvamanrique@gmail.com>
2023-11-30 20:17:57 -0500

    Refactor CategoriesPage component and update CategoryDetail interface

--- src/components/CategoriesPage/CategoriesPage.tsx
+++ src/components/CategoriesPage/CategoriesPage.tsx
@@ -5,25 +5,15 @@ import {
   Heading,
   SimpleGrid,
   Box,
-  Container,
   Flex,
-  Input,
   Select,
   Button,
-  RangeSlider,
-  RangeSliderTrack,
-  RangeSliderFilledTrack,
-  RangeSliderThumb,
-  filter,
   useColorMode,
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useAppSelector } from "../../context/hooks";
-import {
-  getBooksByCategory,
-  getByCategoryPage,
-} from "../../services/BookService";
+import { getByCategoryPage } from "../../services/BookService";
 import { Book } from "../../types/product";
 import CustomLoading from "../CustomLoading/CustomLoading";
 import BreadcrumbNav from "./BreadcrumbNav";
@@ -33,8 +23,9 @@ import { SearchIcon } from "@chakra-ui/icons";
 
 export const Categories = () => {
   const [page, setPage] = useState<number>(0);
-  const [size] = useState<number>(0);
+  const [size] = useState<number>(10); // Cambia esto a la cantidad de elementos que deseas por página
   const [totalElements, setTotalElements] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
   const { categoryName } = useParams();
   const [librosCategoria, setLibrosCategoria] = useState<Book[]>([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -45,77 +36,26 @@ export const Categories = () => {
   const [titleSearch, setTitleSearch] = useState<string>("");
   const [authorSearch, setAuthorSearch] = useState<string>("");
   const [priceSearch, setPriceSearch] = useState<string>("");
+  const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
   const [orderDirection, setOrderDirection] = useState<string>("");
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const [reloadKey, setReloadKey] = useState(0);
+  const [shouldFilter, setShouldFilter] = useState(false);
 
-  useEffect(() => {
+  const fetchCategory = async () => {
     setIsLoading(true);
 
-    const fetchCategory = async () => {
-      try {
-        const response = await getByCategoryPage({
-          page,
-          nameCategory: categoryName,
-        });
-        if (response.statusCode === 200 && response.data) {
-          setTotalElements(response.totalElements ?? 0);
-          setLibrosCategoria(response.data);
-        } else {
-          console.error("Failed to fetch users:", response.errorMessage);
-        }
-      } catch (error) {
-        console.error("Failed to fetch users:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchCategory();
-  }, [reloadKey, page, size]);
-
-  /* if (categoryName) {
-      getBooksByCategory(categoryName, page).then((res) => {
-        setLibrosCategoria(res.content);
-        setTotalElements(res.totalElements)
-        setIsLoading(false);
-      });
-    }
-  }, [categoryName, page, size]);  */
-
-  /* const handleSearch = async () => {
-    setIsLoading(true);
     try {
-      const response = await getByCategoryPage(
-        orderDirection
-      )
-      if (response.statusCode === 200 && response.data) {
-        setTotalElements(response.totalElements ?? 0);
-        setLibrosCategoria(response.data);
-      } else {
-        console.error("Failed to fetch users:", response.errorMessage);
-      }
-    } catch (error) {
-      console.error("Failed to fetch users:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  }; */
-
-  /* const handleClear = async () => {
-    setNameSearch("");
-    setLastNameSearch("");
-    setEmailSearch("");
-    setStatus("");
-    setOrderBy("");
-    setOrderDirection("");
-    setIsLoading(true);
-    try {
-      const response = await getAllUsers({
+      const response = await getByCategoryPage({
         page,
         size,
+        nameCategory: categoryName,
       });
       if (response.statusCode === 200 && response.data) {
+        setTotalPages(response.totalPages ?? 0);
         setTotalElements(response.totalElements ?? 0);
-        setUsers(response.data);
+        setLibrosCategoria(response.data);
+        setFilteredBooks(response.data);
       } else {
         console.error("Failed to fetch users:", response.errorMessage);
       }
@@ -124,7 +64,64 @@ export const Categories = () => {
     } finally {
       setIsLoading(false);
     }
-  }; */
+  };
+  useEffect(() => {
+    fetchCategory();
+  }, [page, size]);
+
+  const handleOptionChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const newValue = event.target.value as string;
+    setSelectedOption(newValue);
+    setOrderBy(newValue);
+  };
+
+  useEffect(() => {
+    if (!shouldFilter) return;
+
+    const sortedBooks = [...librosCategoria].sort((a: Book, b: Book) => {
+      let comparison = 0;
+      switch (selectedOption) {
+        case "title":
+          comparison = a.title.localeCompare(b.title);
+          break;
+        case "authors":
+          comparison = a.authors[0].name.localeCompare(b.authors[0].name);
+          break;
+        case "price":
+          comparison = a.price - b.price;
+          break;
+        case "published_date":
+          comparison =
+            new Date(b.published_date).getTime() -
+            new Date(a.published_date).getTime();
+          break;
+        default:
+          break;
+      }
+      if (orderDirection === "DESC") {
+        comparison *= -1;
+      }
+
+      return comparison;
+    });
+
+    setFilteredBooks(sortedBooks);
+    setShouldFilter(false);
+  }, [shouldFilter, selectedOption, orderDirection, librosCategoria]);
+
+  const handleFilter = () => {
+    setShouldFilter(true);
+  };
+
+  const clearFilters = () => {
+    setOrderBy("");
+    setTitleSearch("");
+    setAuthorSearch("");
+    setPriceSearch("");
+    setOrderDirection("");
+    setSelectedOption(null);
+    setFilteredBooks(librosCategoria); // Restablece los libros filtrados a la lista original de libros
+  };
 
   if (isLoading)
     return (
@@ -172,19 +169,19 @@ export const Categories = () => {
           w="full"
         >
           <Select
+            onChange={handleOptionChange}
             fontSize={fontSize}
             placeholder="Ordenar por"
             value={orderBy}
             focusBorderColor="teal.500"
-            onChange={(e) => setOrderBy(e.target.value)}
             mr={2}
             mb={{ base: 2, md: 0 }}
             w="full"
           >
-            <option value="NAME">Nombre</option>
-            <option value="AUTHOR">Author</option>
-            <option value="PRICE">Price</option>
-            <option value="RECIEN">Recientes</option>
+            <option value="title">Nombre</option>
+            <option value="authors">Author</option>
+            <option value="price">Price</option>
+            <option value="published_date">Recientes</option>
           </Select>
 
           <Select
@@ -210,12 +207,12 @@ export const Categories = () => {
             _hover={{
               bg: "brand.violetLogo75",
             }}
-            //onClick={handleSearch}
+            onClick={handleFilter}
           >
-            Buscar
+            Filtrar
           </Button>
           <Button
-            //onClick={handleClear}
+            onClick={clearFilters}
             colorScheme="gray"
             fontSize={fontSize}
             ml={2}
@@ -230,9 +227,9 @@ export const Categories = () => {
         </Flex>
 
         <Center mb="5vh">
-          {librosCategoria.length > 0 ? (
+          {filteredBooks.length > 0 ? (
             <SimpleGrid columns={{ base: 1, md: 3, lg: 4 }} gap={5}>
-              {librosCategoria.map((producto) => (
+              {filteredBooks.map((producto) => (
                 <ProductCard {...producto} />
               ))}
             </SimpleGrid>
@@ -245,7 +242,6 @@ export const Categories = () => {
           )}
         </Center>
       </Box>
-
       <Pagination
         pageNumber={page}
         pageSize={size}
--- src/types/category.ts
+++ src/types/category.ts
@@ -1,23 +1,21 @@
-import { Product } from "./product"
+import { Product } from "./product";
 
 export interface Category {
-  img: string,
-  title: string
+  img: string;
+  title: string;
 }
 
 export interface CategoryDetail extends Category {
-  products: Product[]
+  products: Product[];
 }
-
 
 export interface GetAllCategoryParams {
   page?: number;
   size?: number;
-  orderDirection?: string;
-  orderBy?: string;
-  title?: string;
-  author?: string;
-  price?: number;
-  status?: string;
   nameCategory?: string;
+  keyword?: string;
+  publisher?: string;
+  releases?: boolean;
+  selection?: string;
+  sort?: string;
 }
